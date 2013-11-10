c5e8d9f79c05d97fa7ae60630b858312729fed50
Noah Stebbins <nsteb1993@gmail.com>
2013-11-09 19:47:57 -0500

    Dashboard
    Resized the image appropriately -- Noah

--- templates/dashboard.php
+++ templates/dashboard.php
@@ -61,8 +61,8 @@
 					<div class="row-fluid">
 						<div class="span6">
 								<h3 class="page-subtitle" style="font-weight: normal" id="questions-header"><?php echo $question[0] ?></h3>
-								<div style="display: block; border-radius: 50%; margin-left: auto; margin-right: auto; height: 200px; 
-								width: 200px; overflow:hidden">
+								<div style="display: block; border-radius: 50%; margin-left: auto; margin-right: auto; height: 300px; 
+								width: 300px; overflow:hidden">
 	      							<img src="../img/profpic-sample1.jpg"/> 
 	      						</div>	
 	
