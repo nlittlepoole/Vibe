fdab59f4048a372396ea5a6efb07e4a97f196827
nlittlepoole <nl2418@columbia.edu>
2014-01-12 14:52:17 -0500

    Bug Fix comments remove
    Fixed bug where users could remove comments of other users

--- classes/Web/String.php
+++ classes/Web/String.php
@@ -15,6 +15,69 @@ function keywords($keywords,$total,$split){
   }
   return $new_keyword!=''?$new_keyword:"N/A";
 }
+function dashboardComments($comments){
+  $comments=split('&&',$comments);
+  $max=sizeof($comments);
+  if($comments[0]!=''){
+    for ($x=0; $x<$max; $x++){
+      $temp=split('##',$comments[$x]);
+      print_r($temp);
+      $datetime2 = new DateTime(date("Y-m-d H:i:s", time()));
+      $datetime1 = new DateTime($temp[1]);
+      $interval = $datetime1->diff($datetime2);
+      $time=$interval->format('%a days');
+      if($time==='0 days'){
+        $time=$interval->format('%h hrs');
+        if($time==='0 hrs'){
+          $time=$interval->format('%i mins');
+        }       
+      }
+    
+    //splice the comment further to get a stylized comment
+    $commentPair = split(':',$temp[2]); 
+    
+      $comments[$x]='                 
+              <li>
+                    <div class="col1">
+                      <div class="cont">
+                        <div class="cont-col1">
+                          <div class="desc"><span style="color: #0d638f;" class="tooltips" data-container="body" data-original-title="' . $commentPair[0] . '">
+                             '. $commentPair[1].'&nbsp;&nbsp;
+                              <span class="label label-sm" style="background-color: ' . $_SESSION['coloredVibes'][$temp[0]] . ';">'.$temp[0].'&nbsp;</span>
+                          </span>
+                          <a style="color: #FF0000" href="/index.php?action=removeComment&comment='.$x.'">Remove</a></div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col2">
+                      <div class="date">
+                         '.$time.'
+                         
+                      </div>
+                    </div>
+                  </li>';
+      //$comments[$x]=Array($temp[2],$temp[0], $time);
+    }
+  }
+    for ($x=$max; $x<9; $x++){
+      $comments[$x]='                 <li>
+                    <div class="col1">
+                      <div class="cont">
+                        <div class="cont-col1">
+                          <div class="desc">
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col2">
+                      <div class="date">
+                      </div>
+                    </div>
+                    
+                  </li>';
+    }
+  return $comments;  
+}
 function comments($comments){
   $comments=split('&&',$comments);
   $max=sizeof($comments);
@@ -45,7 +108,7 @@ function comments($comments){
                              '. $commentPair[1].'&nbsp;&nbsp;
                               <span class="label label-sm" style="background-color: ' . $_SESSION['coloredVibes'][$temp[0]] . ';">'.$temp[0].'&nbsp;</span>
                           </span>
-                          <a style="color: #FF0000" href="/index.php?action=removeComment&comment='.$x.'">Remove</a></div>
+                          </div>
                         </div>
                       </div>
                     </div>
