c63a35aec7631adcdc3213eafa7f0b51a47655a1
Noah Stebbins <nsteb1993@gmail.com>
2014-01-13 12:46:20 -0500

    Checkbox Settings Festive Commit
    Literally took an hour to fix an error with my JavaScript DOM
    Selection. Kill myself — Noah

--- classes/Web/Settings.php
+++ classes/Web/Settings.php
@@ -22,8 +22,45 @@
 	       $showFriends = 1; 
 	   }
 	   
+	   //UPDATE THE DISABLED DATES OF THE USER
+	   $disabledDates = ""; 
+	   for($i = 0; $i < 14; $i++){
+			$disabledDates = $disabledDates . $_POST['checkbox' . ($i + 1)] . " ";
+	   }
+	   $_SESSION['disabledTest'] = $disabledDates; 
+	   
+	   $disabledDates = str_replace("option", "", $disabledDates);
+	   $disabledDates = substr($disabledDates, 0, -1); //remove trailing space character
+	   $disabledArray = explode(" ", $disabledDates);
+	   
+	   $disableChecker = array(); 
+	   foreach($disabledArray as $disableLoc) { 
+	       $disableChecker[$disableLoc - 1] = date("Y-m-d H:i:s", time()); 
+	   } 
+	   
+	   /*
+	    DEBUGGING CODE
+	   $_SESSION['disableLoc'] = ""; 
+	   $i = 0; 
+	   foreach($disableChecker as $disableElem) {
+	   	   $_SESSION['disableLoc'] .= $i . ">>" . $disableElem . "<br />";
+	   	   $i++; 
+	   }
+	   */
+	   
+	   
 	   //Update the information in the user database based on what settings options the user picked
-	   $sql = "UPDATE user SET blurb='$updatedBlurb', displayLocation='$showLocations', displayBirthdate='$showBirthdate', websiteURL='$websiteLink', showNumFriends='$showFriends' WHERE UID=" . $uid;
+	   $sql = "UPDATE user SET blurb='$updatedBlurb', displayLocation='$showLocations', 
+	   displayBirthdate='$showBirthdate', websiteURL='$websiteLink', showNumFriends='$showFriends', 
+	   attractivenessDisableDate='$disableChecker[0]', affabilityDisableDate='$disableChecker[1]', 
+	   intelligenceDisableDate='$disableChecker[2]', styleDisableDate='$disableChecker[3]', 
+	   promiscuityDisableDate='$disableChecker[4]', humorDisableDate='$disableChecker[5]', 
+	   confidenceDisableDate='$disableChecker[6]', funDisableDate='$disableChecker[7]', 
+	   kindnessDisableDate='$disableChecker[8]', honestyDisableDate='$disableChecker[9]', 
+	   reliabilityDisableDate='$disableChecker[10]', happinessDisableDate='$disableChecker[11]', 
+	   ambitionDisableDate='$disableChecker[12]', humilityDisableDate='$disableChecker[13]' WHERE UID=" . $uid;
+	   
+	   //$_SESSION['disableLoc'] = $sql;
 	   $st = $conn->prepare( $sql );
 	   $st->execute();
 	   
