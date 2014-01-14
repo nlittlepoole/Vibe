56216736f21e4d67587aa70e8a3cfd382b72874f
nlittlepoole <nl2418@columbia.edu>
2014-01-13 20:08:25 -0500

    Comments changed from ASCII to ASCII Extended
    ASCII extended enabled in order to support symbols like Ñ from other
    western languages like Spanish

--- classes/Web/Question.php
+++ classes/Web/Question.php
@@ -116,7 +116,7 @@ function submit($facebook,$uid,$token ){
     $null=isset( $_SESSION['null'] ) ? $_SESSION['null'] + "" : "";
     $slider=2*$_POST["slideVal"]; //Slider value needs to be multiplied by two since slider has 5 notches
     $comment=isset( $_POST["commentsVal"] ) && $_POST["commentsVal"]!="" ? $attribute ."##" .date("Y-m-d H:i:s", time())."##". $_SESSION['question'] . ": " .'"' . str_replace(array("|",'"',"##","&&",":"),'',$_POST["commentsVal"]) . '"' : "";
-    $comment=iconv("UTF-8", "ASCII", $comment);
+    $comment=iconv("UTF-8", "ISO_8859-1", $comment);
     $comment=str_replace("'","***",$comment);
     $comment=str_replace(":","{(!)}",$comment);
     $name=isset($_SESSION['Name']) ?$_SESSION['Name']:"Unknown";
