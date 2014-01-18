a24127a35bb19d275abf8d43ef33de60606ee131
Noah Stebbins <nsteb1993@gmail.com>
2014-01-18 01:13:49 -0500

    Footers fixed on all webpages
    Might create footer file if more updates are necessary. — Noah

--- website/achievements.php
+++ website/achievements.php
@@ -273,17 +273,12 @@
 <!-- END CONTAINER -->
 <!-- BEGIN FOOTER -->
 <div class="footer">
-	<div class="footer-inner">
-		 2013 &copy; Metronic by keenthemes.  &nbsp;&nbsp;&nbsp;  2014 &copy; Vibe LLC. &nbsp;&nbsp;&nbsp;
-
-		 <a href="/website/terms.php">Terms and Conditions</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		 <a href="/website/privacy.php">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;
-	</div>
-	<div class="footer-tools">
-		<span class="go-top">
-			<i class="fa fa-angle-up"></i>
-		</span>
-	</div>
+		 <p style="text-align: center; color: #999999;">
+			 2014 &copy; Vibe LLC. &nbsp;&nbsp;&nbsp;
+	
+			 <a href="/website/terms.php">Terms and Conditions</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			 <a href="/website/privacy.php">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;
+		 </p>
 </div>
 <!-- END FOOTER -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
--- website/communities.php
+++ website/communities.php
@@ -1875,14 +1875,12 @@ Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-templa
 <!-- END CONTAINER -->
 <!-- BEGIN FOOTER -->
 <div class="footer">
-	<div class="footer-inner">
-		 2013 &copy; Metronic by keenthemes.
-	</div>
-	<div class="footer-tools">
-		<span class="go-top">
-			<i class="fa fa-angle-up"></i>
-		</span>
-	</div>
+		 <p style="text-align: center; color: #999999;">
+			 2014 &copy; Vibe LLC. &nbsp;&nbsp;&nbsp;
+	
+			 <a href="/website/terms.php">Terms and Conditions</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+			 <a href="/website/privacy.php">Privacy Policy</a>&nbsp;&nbsp;&nbsp;&nbsp;
+		 </p>
 </div>
 <!-- END FOOTER -->
 <!-- BEGIN JAVASCRIPTS(Load javascripts at bottom, this will reduce page load time) -->
