3c1bd0d487f24556ef9c7fec9934478fc343b4ad
nlittlepoole <nl2418@columbia.edu>
2014-01-21 16:50:19 -0500

    Community Search Fix

--- classes/Web/Search.php
+++ classes/Web/Search.php
@@ -135,12 +135,21 @@ function everyone($query){
     return isset($users) ? $users : Array();
 }
 function communities($query){
+    $queries=split(' ',$query);
+    $query='';
+    for($x=0;$x<sizeof($queries);$x++){ 
+        if($queries[$x]){
+            $query=$query." OR Name LIKE '%". $queries[$x] ."%'";
+        }
+    }
+    $query=substr($query,3);
     $conn = new PDO( DB_DSN, DB_USERNAME, DB_PASSWORD );
-    $sql = "SELECT Name, UID FROM directory WHERE Name LIKE '%".$query."%' LIMIT 50"; //gets the active status of the user with $input_id as a user ID
+    $sql = "SELECT Name, UID FROM directory WHERE ".$query." LIMIT 50"; //gets the active status of the user with $input_id as a user ID
     $st = $conn->prepare( $sql ); //this is a useful security line, hides the sql commands from browser consoles
     $st->execute(); //executes the sql query found above
     $conn = null;
     $communities=$st->fetchAll();
+
     for($x=0;$x<sizeof($communities);$x++){
         $communities[$x]= '<tr><td><a href="/index.php?action=location&location='.$communities[$x]['UID'].'""><img src="http://graph.facebook.com/'.$communities[$x]['UID'].'/picture" height="62" width="62" /></a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="/index.php?action=location&location='.$communities[$x]['UID'].'"">'.$communities[$x]['Name'].'</a></td></tr>'  ;
     }
