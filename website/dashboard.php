2586a97d2ad1f4ce6262b8bb0c788b38c43bc126
Noah Stebbins <nsteb1993@gmail.com>
2014-01-24 20:23:23 -0500

    Some changes to speed up site
    — Noah

--- website/dashboard.php
+++ website/dashboard.php
@@ -52,9 +52,6 @@
 <!-- END THEME STYLES -->
 <link rel="shortcut icon" href="favicon.ico"/>
 
-<!-- jQuery -->
-<script src="../jQuery/jquery.js"></script> 
-
 <!-- JOYRIDE INCLUDE FILES -->
 <!-- Attach the Joyride CSS file -->
 <link rel=" stylesheet" type="text/css" href="../zurb-joyride-9685a1e/joyride-2.1.css">
@@ -645,8 +642,9 @@
 <script src="assets/plugins/respond.min.js"></script>
 <script src="assets/plugins/excanvas.min.js"></script> 
 <![endif]-->
+<!--
 <script src="assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>-->
 <!-- IMPORTANT! Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
 <script src="assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
@@ -657,6 +655,7 @@
 <script src="assets/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 <!-- BEGIN PAGE LEVEL PLUGINS -->
+<!--
 <script src="assets/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
 <script src="assets/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
 <script src="assets/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
@@ -669,12 +668,15 @@
 <script src="assets/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
+-->
 <!-- Commented out below so the pop-up notifications don't happen! (Noah)-->
 <!-- <script src="assets/plugins/gritter/js/jquery.gritter.js" type="text/javascript"></script> -->
 <!-- IMPORTANT! fullcalendar depends on jquery-ui-1.10.3.custom.min.js for drag & drop support -->
+<!--
 <script src="assets/plugins/fullcalendar/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
 <script src="assets/plugins/jquery-easy-pie-chart/jquery.easy-pie-chart.js" type="text/javascript"></script>
 <script src="assets/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+-->
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
 <script src="assets/scripts/app.js" type="text/javascript"></script>
