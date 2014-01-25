73c307c8ff9afc44d6785bf6bd864e6be5219827
Noah Stebbins <nsteb1993@gmail.com>
2014-01-24 23:48:30 -0500

    minified dashboard_new
    — Noah

--- website/dashboard_new.php
+++ website/dashboard_new.php
@@ -35,11 +35,12 @@
 <link href="assets/plugins/uniform/css/uniform.default.css" rel="stylesheet" type="text/css"/>
 <!-- END GLOBAL MANDATORY STYLES -->
 <!-- BEGIN PAGE LEVEL PLUGIN STYLES -->
+<!--
 <link href="assets/plugins/gritter/css/jquery.gritter.css" rel="stylesheet" type="text/css"/>
 <link href="assets/plugins/bootstrap-daterangepicker/daterangepicker-bs3.css" rel="stylesheet" type="text/css"/>
 <link href="assets/plugins/fullcalendar/fullcalendar/fullcalendar.css" rel="stylesheet" type="text/css"/>
 <link href="assets/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css"/>
-<link href="assets/plugins/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/jquery-easy-pie-chart/jquery.easy-pie-chart.css" rel="stylesheet" type="text/css"/>-->
 <!-- END PAGE LEVEL PLUGIN STYLES -->
 <!-- BEGIN THEME STYLES -->
 <link href="assets/css/style-metronic.css" rel="stylesheet" type="text/css"/>
@@ -51,9 +52,6 @@
 <link href="assets/css/custom.css" rel="stylesheet" type="text/css"/>
 <!-- END THEME STYLES -->
 <link rel="shortcut icon" href="favicon.ico"/>
-
-<!-- jQuery -->
-<script src="../jQuery/jquery.js"></script> 
 
 <!-- JOYRIDE INCLUDE FILES -->
 <!-- Attach the Joyride CSS file -->
@@ -687,18 +685,19 @@
 <script src="assets/plugins/respond.min.js"></script>
 <script src="assets/plugins/excanvas.min.js"></script> 
 <![endif]-->
-<script src="assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>
+<!--<script src="assets/plugins/jquery-1.10.2.min.js" type="text/javascript"></script>-->
+<!--<script src="assets/plugins/jquery-migrate-1.2.1.min.js" type="text/javascript"></script>-->
 <!-- IMPORTANT! Load jquery-ui-1.10.3.custom.min.js before bootstrap.min.js to fix bootstrap tooltip conflict with jquery ui tooltip -->
 <script src="assets/plugins/jquery-ui/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap-hover-dropdown/twitter-bootstrap-hover-dropdown.min.js" type="text/javascript"></script>
 <script src="assets/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery.blockui.min.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery.cokie.min.js" type="text/javascript"></script>
+<!--<script src="assets/plugins/jquery.blockui.min.js" type="text/javascript"></script>-->
+<!--<script src="assets/plugins/jquery.cokie.min.js" type="text/javascript"></script>-->
 <script src="assets/plugins/uniform/jquery.uniform.min.js" type="text/javascript"></script>
 <!-- END CORE PLUGINS -->
 <!-- BEGIN PAGE LEVEL PLUGINS -->
+<!--
 <script src="assets/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
 <script src="assets/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
 <script src="assets/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
@@ -710,13 +709,14 @@
 <script src="assets/plugins/flot/jquery.flot.resize.js" type="text/javascript"></script>
 <script src="assets/plugins/jquery.pulsate.min.js" type="text/javascript"></script>
 <script src="assets/plugins/bootstrap-daterangepicker/moment.min.js" type="text/javascript"></script>
-<script src="assets/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap-daterangepicker/daterangepicker.js" type="text/javascript"></script>-->
 <!-- Commented out below so the pop-up notifications don't happen! (Noah)-->
 <!-- <script src="assets/plugins/gritter/js/jquery.gritter.js" type="text/javascript"></script> -->
 <!-- IMPORTANT! fullcalendar depends on jquery-ui-1.10.3.custom.min.js for drag & drop support -->
+<!--
 <script src="assets/plugins/fullcalendar/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
 <script src="assets/plugins/jquery-easy-pie-chart/jquery.easy-pie-chart.js" type="text/javascript"></script>
-<script src="assets/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery.sparkline.min.js" type="text/javascript"></script>-->
 <!-- END PAGE LEVEL PLUGINS -->
 <!-- BEGIN PAGE LEVEL SCRIPTS -->
 <script src="assets/scripts/app.js" type="text/javascript"></script>
@@ -727,12 +727,12 @@
 jQuery(document).ready(function() {    
    App.init(); // initlayout and core plugins
    Index.init();
-   Index.initJQVMAP(); // init index page's custom scripts
-   Index.initCalendar(); // init index page's custom scripts
-   Index.initCharts(); // init index page's custom scripts
-   Index.initChat();
+   //Index.initJQVMAP(); // init index page's custom scripts
+   //Index.initCalendar(); // init index page's custom scripts
+   //Index.initCharts(); // init index page's custom scripts
+   //Index.initChat();
    Index.initMiniCharts();
-   Index.initDashboardDaterange();
+   //Index.initDashboardDaterange();
    Index.initIntro();
    Tasks.initDashboardWidget();
 });
