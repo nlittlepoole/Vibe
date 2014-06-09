33ce2f4ade78ca2675cdb0c81ff8ea334012f44b
Niger Little-Poole <nl2418@columbia.edu>
2014-06-09 09:07:06 -0700

    Resset Vibe Social with new Files

--- config.php
+++ config.php
@@ -1,16 +1,21 @@
+
 <?php
 ini_set( "display_errors", true );
 $adress=getenv('IP');
+define( "ROOT", $_SERVER['DOCUMENT_ROOT']);
 date_default_timezone_set( "America/New_York" );  // http://www.php.net/manual/en/timezones.php
-define( "DB_DSN", "mysql:host=localhost;dbname=Vibosphere" );
-define( "DB_USERNAME", "Vibosphere" );
+define( "DB_DSN", "mysql:host=79afe5225f7fd454174526fe4108092758d4e751.rackspaceclouddb.com;dbname=VibeSocial" );
+define( "DB_USERNAME", "nlittlepoole" );
 define( "DB_PASSWORD", "Carman4ever!" );
 define( "CLASS_PATH", "classes" );
 define( "ADMIN_USERNAME", "teamvibe" );
 define( "ADMIN_PASSWORD", "Carman4ever" );
-define( "APP_ID", "257473684410281" );
-define( "APP_SECRET", "beeb8e7913c442c8ae1dd08bb05d74d5" );
- 
+define( "APP_ID", "246588708881137" );
+define( "APP_SECRET", "8934166537a59f745ccd26e29f4ef781" );
+$token=(string)file_get_contents("https://graph.facebook.com/oauth/access_token?%20client_id=246588708881137&client_secret=8934166537a59f745ccd26e29f4ef781&grant_type=client_credentials", true);
+$token=str_replace('access_token=','',$token);
+define( "APP_TOKEN", $token );
+
 function handleException( $exception ) {
   echo "Sorry, a problem occurred. Please try later.";
   echo $exception->getMessage();
