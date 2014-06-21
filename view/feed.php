651ee4062c2493b00b6a63296510e61859922bd9
Niger Little-Poole <nl2418@columbia.edu>
2014-06-21 08:53:09 -0700

    Header.php
    Header.php starts session on front end pages. It checks if the user is
    logged in and also checks if certain cached variables are set and fills
    them if they are not

--- view/feed.php
+++ view/feed.php
@@ -1,6 +1,5 @@
 <?php 
-session_start();
-$_SESSION['token'];
+require_once("header.php");
 ?>
 <html>
 <head>
@@ -16,14 +15,7 @@ $_SESSION['token'];
 <form name="status" action="http://niger.go-vibe.com/api/vibe.php?action=postVibe" method="post">
 	<select name="recipient">
 <?php 
-	$request = "http://niger.go-vibe.com/api/user.php?action=getFriends&uid=". $_SESSION['userID'];
-	$request = $request."&token=".$_SESSION['token'];
-	$friends = json_decode(file_get_contents($request),true);
-	$friends = $friends['data'];
-	usort( $friends , function( $a ,$b ){
-		return strcmp($a['Name'],$b['Name']);
-	});
-	foreach ($friends as $person){
+	foreach ($_SESSION['friend_list'] as $person){
 		if($person['UID']!=$_SESSION['userID']){
 			echo'<option value="'.$person['UID'].'">'.$person['Name'].'</option>';
 		}
---
+++ view/header.php
@@ -0,0 +1,18 @@
+<?php 
+session_start();
+//go home if user isn't logged in
+if(!isset($_SESSION['userID'])){
+	header("Location:http://niger.go-vibe.com");
+}
+//store friends in session to speed up feed reloads
+if(!isset($_SESSION['friend_list'])){
+	$request = "http://niger.go-vibe.com/api/user.php?action=getFriends&uid=". $_SESSION['userID'];
+	$request = $request."&token=".$_SESSION['token'];
+	$friends = json_decode(file_get_contents($request),true);
+	$friends = $friends['data'];
+	usort( $friends , function( $a ,$b ){
+		return strcmp($a['Name'],$b['Name']);
+	});
+	$_SESSION['friend_list']=$friends;
+}
+?>
