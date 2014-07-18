bdd07f4fafed347833d1991b47e72eb19e410c03
Noah Stebbins <nsteb1993@gmail.com>
2014-07-17 21:02:40 -0400

    Newsfeed streaming enabled!
    Finished streaming of remaining elements on newsfeed!
    
    — Noah

--- social-v2.0.0/admin_fixed/newsfeed_element.php
+++ social-v2.0.0/admin_fixed/newsfeed_element.php
@@ -24,7 +24,9 @@
 		        	// grab the first element and store it in session data
 		        	if(i == 0) {
 
-		        		most_recent_pid = String(data['data'][i]['PID']); 
+		        		// debugger; 
+
+		        		var most_recent_pid = String(data['data'][0]['PID']); 
 
 		        		// alert('most recent pid is ' + most_recent_pid + ' and local storaged value is now ' + String(localStorage.getItem("latest_pid")));
 		        		// debugger; 
@@ -34,20 +36,25 @@
 		        			// debugger; 
 
 		        			if(localStorage.getItem("latest_pid") == most_recent_pid) {
-		        				alert('not doing any append update!');
+		        				// alert('not doing any append update!');
 		        				return; 
 		        			}
 		        			else {
-		        				alert('we have triggered a difference!');
+		        				// alert('we have triggered a difference!');
 		        			
 			        			// NOTE: the returned content is different than the rendered content!
-			        			curr_pid = most_recent_pid
-			        			var j = i; 
+			        			var curr_pid = most_recent_pid
+			        			var j = 0; 
 
-			        			while(curr_pid != localStorage.getItem("latest_pid")) {
+			        			var html_newsfeed_content = ""; 
+
+			        			// debugger; 
+			        			// alert("current pid: " + curr_pid + ", local storage item: " + localStorage.getItem("latest_pid"));
+			        			while(String(curr_pid) !== String(localStorage.getItem("latest_pid"))) {
+			        				// alert("current pid: " + curr_pid + ", local storage item: " + localStorage.getItem("latest_pid"));
 			        				var temp_link = "http://api.go-vibe.com/social-v2.0.0/admin_fixed/new_profile.php?user=" + data['data'][j]['Tagged'] + "&name=" + data['data'][j]['Name'] + "";
 
-			        				var html_newsfeed_content = 
+			        				html_newsfeed_content += 
 						        		["<li class='active vibe_newsfeed_posts'>", 
 											"<span class='marker'></span>",
 											"<div class='block'>",
@@ -83,13 +90,15 @@
 										"</li>",
 										].join('\n');
 
-									$('#newsfeed_container').prepend(html_newsfeed_content);
-
-									curr_pid = data['data'][j]['PID'];
-			        				j += 1; 
+									j += 1;
+									curr_pid = data['data'][j]['PID']; 
 			        			}
 
-			        			most_recent_pid = data['data'][j]['PID'];
+			        			// debugger; 
+			        			$('#newsfeed_container').prepend(html_newsfeed_content);
+			        			// debugger; 
+
+			        			most_recent_pid = data['data'][0]['PID'];
 		        				localStorage.setItem("latest_pid", most_recent_pid);
 		        				// alert('leaving after difference triggering!');
 		        				return; 
@@ -99,7 +108,7 @@
 		        			// alert("you made it to the else!");
 		        			// debugger; 
 		        			// first time going through
-		        			most_recent_pid = data['data'][i]['PID'];
+		        			most_recent_pid = data['data'][0]['PID'];
 		        			localStorage.setItem("latest_pid", most_recent_pid);
 		        			// alert('IMPORTANT: localstorage item set to ' + most_recent_pid); 
 		        		} 
@@ -152,7 +161,7 @@
 						"</li>",
 						].join('\n');
 
-					$('#newsfeed_container').prepend(html_newsfeed_content);
+					$('#newsfeed_container').append(html_newsfeed_content);
 		        }
 		    } 
 		    else {
