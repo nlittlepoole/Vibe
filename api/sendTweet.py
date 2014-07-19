44fbeafd020b1faa988115e6d43e0f2211621746
Niger Little-Poole <nl2418@columbia.edu>
2014-07-19 10:14:43 -0700

    Vibe Twitter Bot upgraded
    Twitter bot now has shortened referal url and takes in users to mention
    on twitter

--- api/sendTweet.py
+++ api/sendTweet.py
@@ -1,7 +1,10 @@
 import tweepy, sys
- 
-tweet = str(sys.argv[1])
- 
+import requests
+import simplejson as json
+
+tweet = str(sys.argv[3])
+url = str(sys.argv[2])
+user = str(sys.argv[1])
 #enter the corresponding information from your Twitter application:
 ACCESS_KEY = '2658347198-pA2na530L78WvwSAfpXfUkhOfInmxUIePf5qsc3'#keep the quotes, replace this with your consumer key
 ACCESS_SECRET = 'vPNdOPZnXsU9Vl5tJ3YFpqr30vtVIhkUfYdim587nDBMP'#keep the quotes, replace this with your consumer secret key
@@ -9,6 +12,17 @@ CONSUMER_KEY = 't6k0VxPwu6RFoUBMr3r7nxRCT'#keep the quotes, replace this with yo
 CONSUMER_SECRET = 'bjX8wtT1ubxJDWH0q2oFoZnhUJcCBJqyicHChtGpHENEqX9JfZ'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+# Shorten URL
+gUrl = 'https://www.googleapis.com/urlshortener/v1/url?fields=id'
+data = json.dumps({'longUrl': url})
+r = requests.post(gUrl, data, headers={'Content-Type': 'application/json'})
+url = json.loads( r.text).get('id').replace('http://','')
+
+# Trim and combine tweet
+cut=128 - len(user) - len(url )
+tweet = user + " " + tweet[0:cut] + "... " +url
+
+#print tweet
 api = tweepy.API(auth)
- 
 api.update_status(tweet)
