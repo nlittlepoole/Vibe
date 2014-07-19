8a61b84f5af7ef3555c13c3065c59207d0965580
Niger Little-Poole <nl2418@columbia.edu>
2014-07-19 09:35:47 -0700

    Switched the access keys

--- api/sendTweet.py
+++ api/sendTweet.py
@@ -3,10 +3,10 @@ import tweepy, sys
 tweet = str(sys.argv[1])
  
 #enter the corresponding information from your Twitter application:
-CONSUMER_KEY = '2658347198-pA2na530L78WvwSAfpXfUkhOfInmxUIePf5qsc3'#keep the quotes, replace this with your consumer key
-CONSUMER_SECRET = 'vPNdOPZnXsU9Vl5tJ3YFpqr30vtVIhkUfYdim587nDBMP'#keep the quotes, replace this with your consumer secret key
-ACCESS_KEY = 't6k0VxPwu6RFoUBMr3r7nxRCT'#keep the quotes, replace this with your access token
-ACCESS_SECRET = 'bjX8wtT1ubxJDWH0q2oFoZnhUJcCBJqyicHChtGpHENEqX9JfZ'#keep the quotes, replace this with your access token secret
+ACCESS_KEY = '2658347198-pA2na530L78WvwSAfpXfUkhOfInmxUIePf5qsc3'#keep the quotes, replace this with your consumer key
+ACCESS_SECRET = 'vPNdOPZnXsU9Vl5tJ3YFpqr30vtVIhkUfYdim587nDBMP'#keep the quotes, replace this with your consumer secret key
+CONSUMER_KEY = 't6k0VxPwu6RFoUBMr3r7nxRCT'#keep the quotes, replace this with your access token
+CONSUMER_SECRET = 'bjX8wtT1ubxJDWH0q2oFoZnhUJcCBJqyicHChtGpHENEqX9JfZ'#keep the quotes, replace this with your access token secret
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
