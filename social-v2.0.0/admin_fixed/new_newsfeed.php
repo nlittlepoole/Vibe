c75767890ce588ede17fba4c0d743c4cafb478c7
Noah Stebbins <nsteb1993@gmail.com>
2014-07-23 00:01:21 -0400

    Newsfeed //
    finished with re-structuring of Newsfeed
    — Noah

--- social-v2.0.0/admin_fixed/new_newsfeed.php
+++ social-v2.0.0/admin_fixed/new_newsfeed.php
@@ -130,6 +130,7 @@
 	  }
 
 	</style>
+
 </head>
 
 <body class=" scripts-async menu-right-hidden">
@@ -138,109 +139,121 @@
 	<div class="container-fluid ">
 
 		
-		<!-- Content START -->
+		<!-- Content Start -->
 		<div id="content">
 			
-			<!-- NAVBAR LOAD -->	
+			<!-- loading NAVBAR... -->	
 			<script> 
 			    $(function(){
 			      $('#new_navbar').load('new_navbar.php'); 
 			    });
 			</script> 
+
 			<div id="new_navbar"></div>
 
+			<div class="container"><div class="innerAll">
+				<div class="row">
+					<div class="col-lg-9 col-md-8">
+						
+						<div class="timeline-cover">
 
-		<div class="container"><div class="innerAll">
-			<div class="row">
-				<div class="col-lg-9 col-md-8">
-					
-					<div class="timeline-cover">
+							<!-- VIBE SUBMIT FORM -->
+							<div class="widget widget-heading-simple widget-body-white">
+								
+								<div class="widget-body">
+									<div class="innerLR">
+										<div class="col-sm-12" style="padding: 0px; margin-bottom: 15px;">
+											
+											<!-- header or greeting -->
+											<div class="bg-gray innerAll border-top border-bottom">
+												<h4 style="text-align: center; color: #428bca; margin-top: 5px; margin-bottom: 5px;" class="heading"><?php echo $_SESSION['first_name']; ?>, what's on your mind?</h4>
+											</div>
 
-						<!-- WIDGET -->
-						<div class="widget widget-heading-simple widget-body-white">
-							
-							<div class="widget-body">
-								<div class="innerLR">
-									<div class="col-sm-12" style="padding: 0px; margin-bottom: 15px;">
-										
-										<div class="bg-gray innerAll border-top border-bottom">
-											<h4 style="text-align: center; color: #428bca; margin-top: 5px; margin-bottom: 5px;" class="heading"><?php echo $_SESSION['first_name']; ?>, what's on your mind?</h4>
 										</div>
+										<form name="status" id="statusform" class="form-horizontal" method="post" action="http://api.go-vibe.com/api/vibe.php?action=postVibe">
+										    
+											<!-- friend selection -->
+										    <div class="form-group">
+										        <label for="inputFriend" class="col-sm-2 control-label">Friend</label>
+										        
+										        <div class="col-sm-9">
+										            <input type="text" class="form-control" id="inputFriend" name="recipient_to_convert" placeholder="Who's this about? Type in a Facebook friend!">
+										        	<input type="hidden" value="" name="recipient" />
+										        	<input type="hidden" value="newsfeed" name="post_source" />
+										        </div>
 
+										    </div>
+
+										    <!-- hidden fields -->
+										    <div class="form-group">
+										    	<input type="hidden" name="uid" value=<?php echo '"' . $_SESSION['userID'] . '"' ?>>
+			    								<input type="hidden" name="token" value=<?php echo '"' . $_SESSION['token'] . '"' ?>>
+			    							</div>
+
+			    							<!-- Vibe content field -->
+										    <div class="form-group">
+										        
+										        <label for="inputVibe" class="col-sm-2 control-label">Vibe</label>
+										        
+										        <div class="col-sm-9">
+										            <input type="text" class="form-control" name="status" id="inputVibe" placeholder="What do you want to say?">
+										        </div>
+
+										    </div>
+
+										    <!-- submit field -->
+										    <div class="form-group">
+
+										        <div class="col-sm-offset-2 col-sm-10">
+										            <button type="submit" value="Submit" id="status_submit" class="btn btn-primary" style="width: 20%">Share&nbsp;&nbsp;<i class="fa fa-arrow-circle-right"></i></button>
+										        </div>
+
+										    </div>
+
+										</form>
 									</div>
-									<form name="status" id="statusform" class="form-horizontal" method="post" action="http://api.go-vibe.com/api/vibe.php?action=postVibe">
-									    <div class="form-group">
-									        <label for="inputFriend" class="col-sm-2 control-label">Friend</label>
-									        
-									        <div class="col-sm-9">
-									            <input type="text" class="form-control" id="inputFriend" name="recipient_to_convert" placeholder="Who's this about? Type in a Facebook friend!">
-									        	<input type="hidden" value="" name="recipient" />
-									        	<input type="hidden" value="newsfeed" name="post_source" />
-									        </div>
-
-									    </div>
-									    <div class="form-group">
-									    	<input type="hidden" name="uid" value=<?php echo '"' . $_SESSION['userID'] . '"' ?>>
-		    								<input type="hidden" name="token" value=<?php echo '"' . $_SESSION['token'] . '"' ?>>
-		    							</div>
-									    <div class="form-group">
-									        
-									        <label for="inputVibe" class="col-sm-2 control-label">Vibe</label>
-									        
-									        <div class="col-sm-9">
-									            <input type="text" class="form-control" name="status" id="inputVibe" placeholder="What do you want to say?">
-									        </div>
-
-									    </div>
-									    <div class="form-group">
-
-									        <div class="col-sm-offset-2 col-sm-10">
-									            <button type="submit" value="Submit" id="status_submit" class="btn btn-primary" style="width: 20%">Share&nbsp;&nbsp;<i class="fa fa-arrow-circle-right"></i></button>
-									        </div>
-
-									    </div>
-									</form>
 								</div>
+							
 							</div>
-						
+							<!-- // Widget END -->
+
+						<!-- end of TIMELINE COVER -->	
 						</div>
-						<!-- // Widget END -->
-
-					<!-- END OF NORMAL TIMELINE -->	
-					</div>
-					
-					<div class="media">
 						
-						<a href="" class="btn btn-default pull-left">Today</a>
-						<div class="media-body">
-							  <div class="input-group">
-							      <input type="text" class="form-control" placeholder="Share your mood...">
-							      <span class="input-group-btn">
-							        <button class="btn btn-primary" type="button">Search</button>
-							      </span>
-							    </div><!-- /input-group -->
+						<!-- HEADER Widget of NEWSFEED ELEMENTS -->
+						<div class="media">
+							
+							<a href="" class="btn btn-default pull-left">Today</a>
+							<div class="media-body">
+								  <div class="input-group">
+								      <input type="text" class="form-control" placeholder="Share your mood...">
+
+								      <span class="input-group-btn">
+								          <button class="btn btn-primary" type="button">Search</button>
+								      </span>
+
+								  </div><!-- /input-group -->
+							</div>
 						</div>
+
+						<!-- actual TIMELINE -->
+						<ul class="timeline-activity list-unstyled" id="newsfeed_container">
+
+							<!-- loading NEWSFEED ELEMENTS... -->
+							
+							<script> 
+							    $(function(){
+							      	$('#last_elems').load('newsfeed_element.php'); 
+							    });
+							</script> 
+
+							<div id="last_elems"></div>
+
+						</ul>
+
 					</div>
 
-					<ul class="timeline-activity list-unstyled" id="newsfeed_container">
-
-						<div id="last_elems"></div>
-
-						<!-- NEWSFEED LOADING INFRASTRUCTURE -->
-						
-						<script> 
-						    $(function(){
-						      	$('#last_elems').load('newsfeed_element.php'); 
-						    });
-						</script> 
-
-
-						<!-- END OF NEWSFEED LOADING INFRASTRUCTURE -->
-
-					</ul>				
-				</div>
-
-					<!-- SIDEBAR WIDGET -->
+					<!-- loading SIDEBAR... -->
 					<div class="col-md-4 col-lg-3">
 
 						<script> 
@@ -248,26 +261,29 @@
 						      $('#new_sidebar').load('new_sidebar.php'); 
 						    });
 						</script> 
+
 						<div id="new_sidebar"></div>
 							
 					</div> 
-						
-			</div>
-			<!-- // Content END -->
-			
-			<div class="clearfix"></div>
-			<!-- // Sidebar menu & content wrapper END -->
-			
-			<!-- Footer -->
-			<div id="footer" class="hidden-print">
+							
+				</div>
+				<!-- // Content END -->
 				
-				<!-- potential footer goes here -->
-		
-			</div>
-			<!-- // Footer END -->
+				<div class="clearfix"></div>
+				<!-- // Sidebar menu & content wrapper END -->
+				
+				<!-- Footer -->
+				<div id="footer" class="hidden-print">
 					
-		</div></div>
-		<!-- // Main Container Fluid END -->
+					<!-- potential footer goes here -->
+			
+				</div>
+				<!-- // Footer END -->
+						
+			</div></div>
+			<!-- // Main Container Fluid END -->
+		</div>
+	</div>
 	
 
 	<!-- global -->
