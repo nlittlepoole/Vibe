15c7b7662afd36b5202f473238fa51507ce59342
Niger Little-Poole <nl2418@columbia.edu>
2014-07-29 15:03:40 -0700

    Auto Delete Enabled
    Auto Deleting of Posts older than 72 hours has been enabled. this date
    is easily changed but for now going with 72 hours. The script runs on a
    cron every hour

--- api/auto_delete.py
+++ api/auto_delete.py
@@ -1,19 +1,21 @@
 import MySQLdb
 
-# connect to database
+# connect to database                                                                                                                                                                                     
 db = MySQLdb.connect(host="79afe5225f7fd454174526fe4108092758d4e751.rackspaceclouddb.com",
-                     port=3306, # your host, usually localhost
-                     user="nlittlepoole", # your username
-                     passwd="Carman4ever!", # your password
-                     db="VibeSocial") # name of the data base
-# Get user ID from argument
-uid = sys.argv[1]
+                     port=3306, # your host, usually localhost                                                                                                                                            
+                     user="nlittlepoole", # your username                                                                                                                                                 
+                     passwd="Carman4ever!", # your password                                                                                                                                               
+                     db="VibeSocial") # name of the data base                                                                                                                                             
 
-# Grab vibes for user
+# Get all the expired Posts
 cur = db.cursor()
-cur.execute("SELECT FROM Posts WHERE Type = 'Master' AND Timestamp < (NOW() - INTERVAL 1 DAY);")
-pids= cur.fetchall()
+cur.execute("SELECT PID FROM Posts WHERE Type = 'Master' AND Timestamp < (NOW() - INTERVAL 3 DAY);")
+pids = cur.fetchall()
+
+# Delete Posts and their comments
 for pid in pids:
-	sql = "DELETE FROM Posts WHERE PID=' " +pid + "' ;"
-	cur.execute( sql )
+        sql = "DELETE FROM Posts WHERE PID='" +pid[0] + "' ;"
+        cur.execute( sql )
+        cur.connection.commit()
 db.close()
+
